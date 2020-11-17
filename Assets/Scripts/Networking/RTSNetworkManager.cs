8e005a1312e85414b8f3aefe6b896c32548e890d
Sam Little <kagutaba256@gmail.com>
2020-11-16 18:38:57 -0800

    asdf

---
+++ Assets/Scripts/Networking/RTSNetworkManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+public class RTSNetworkManager : NetworkManager {
+
+  [SerializeField] private GameObject unitSpawnerPrefab = null;
+
+  #region server
+
+  public override void OnServerAddPlayer (NetworkConnection conn) {
+    base.OnServerAddPlayer (conn);
+    GameObject unitSpawnerInstance = Instantiate (
+      unitSpawnerPrefab,
+      conn.identity.transform.position,
+      conn.identity.transform.rotation);
+    NetworkServer.Spawn (unitSpawnerInstance, conn);
+  }
+
+  #endregion
+
+  #region client 
+  #endregion
+}
---
+++ Assets/Scripts/Units/Unit.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Unit : NetworkBehaviour {
+    [SerializeField] private UnityEvent onSelected = null;
+    [SerializeField] private UnityEvent onDeselected = null;
+
+    #region server
+    #endregion
+
+    #region client
+    [Client]
+    public void Select () {
+        if (!hasAuthority) { return; }
+        onSelected?.Invoke ();
+    }
+
+    [Client]
+    public void Deselect () {
+        if (!hasAuthority) { return; }
+        onDeselected?.Invoke ();
+    }
+    #endregion
+}
